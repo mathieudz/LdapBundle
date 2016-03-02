b4f8c7981b4df986a42ae95beb3777d893b0245c
Mathieu De Zutter <mathieu@dezutter.org>
2016-03-02 20:59:26 +0100

    Avoid deprecation warnings in Symfony 2.7

--- EventListener/LdapListener.php
+++ EventListener/LdapListener.php
@@ -9,6 +9,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface,
     Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface,
     Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken,
     Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException,
+    Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface,
     Symfony\Component\Security\Core\SecurityContextInterface,
     Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface,
     Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface,
@@ -19,7 +20,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface,
 
 class LdapListener extends AbstractAuthenticationListener
 {
-    public function __construct(SecurityContextInterface $securityContext,
+    public function __construct(TokenStorageInterface $tokenStorage,
                                 AuthenticationManagerInterface $authenticationManager,
                                 SessionAuthenticationStrategyInterface $sessionStrategy,
                                 HttpUtils $httpUtils,
@@ -32,7 +33,7 @@ class LdapListener extends AbstractAuthenticationListener
                                 CsrfProviderInterface $csrfProvider = null)
     {
         parent::__construct(
-            $securityContext,
+            $tokenStorage,
             $authenticationManager,
             $sessionStrategy,
             $httpUtils,
